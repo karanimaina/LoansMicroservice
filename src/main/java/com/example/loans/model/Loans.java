b840b723b99bbc4d89256712a2b38ac46dbc7850
felixmaina <karani.maina2010@gmail.com>
2022-10-25 10:48:38 +0300

    add a no args constructor

--- src/main/java/com/example/loans/model/Loans.java
+++ src/main/java/com/example/loans/model/Loans.java
@@ -1,4 +1,24 @@
 package com.example.loans.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class Loans {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+   private long id;
+   private Integer amount;
+   private String currency;
 }
